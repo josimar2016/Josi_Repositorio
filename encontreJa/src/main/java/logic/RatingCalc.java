7608d3941ba51d67cb44c9fdf34ac82e0525ba7a
Tiago Coelho <Tiago Coelho>
2016-05-31 23:16:17 +0100

    junit test adicionado

---
+++ encontreJa/src/main/java/logic/RatingCalc.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.util.List;
+import pt.ua.encontreja.entity.Rating;
+
+/**
+ *
+ * @author mezca
+ */
+public class RatingCalc {
+
+    private List<Rating> ratings;
+
+    public RatingCalc(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+    public double getAvgRat() {
+        double avg = 0;
+        int count = 0;
+        for (Rating rat : ratings) {
+            avg += rat.getRatng();
+            count++;
+        }
+
+        return (avg / (double) count);
+    }
+}
---
+++ encontreJa/src/test/java/TestRatingCalc.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import logic.RatingCalc;
+import org.junit.Before;
+import org.junit.Test;
+import pt.ua.encontreja.entity.Rating;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mezca
+ */
+public class TestRatingCalc {
+    
+    private RatingCalc ratingCal;
+    private static final double DELTA = 1e-15;
+    
+    @Before
+    public void setUp() 
+    {
+       
+        List<Rating> ratings = new ArrayList<>();
+        Rating rat1 = new Rating();
+        rat1.setRatng(5);
+        
+        Rating rat2 = new Rating();
+        rat2.setRatng(3);
+        
+        Rating rat3 = new Rating();
+        rat3.setRatng(4);
+        
+        ratings.add(rat1);
+        ratings.add(rat2);
+        ratings.add(rat3);
+        ratingCal = new RatingCalc(ratings);
+        
+    }
+    
+    @Test
+    public void testGetAvgRat() 
+    {
+        
+        double res = ((5+3+4) / 3);
+        assertEquals("average user rating",res, ratingCal.getAvgRat(),DELTA);
+       
+    }
+    
+}
